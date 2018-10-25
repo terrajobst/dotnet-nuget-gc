2e8c57b22245e4ec3498dad1e9a342b9b0e6f6be
Atif Aziz <code@raboof.com>
2018-10-25 09:59:16 +0200

    Do dry-run by default, requiring -f to really purge

--- src/Program.cs
+++ src/Program.cs
@@ -8,8 +8,29 @@ namespace NugetCacheCleaner
     {
         static void Main(string[] args)
         {
-            int minDays = 30;
-            if (args.Length > 0 && int.TryParse(args[0], out minDays)) { }
+            var flags = args.Where(arg => arg.Length > 0 && arg[0] == '-').ToArray();
+            const string forceName = "--force"; const string forceShortName = "-f";
+            var force = flags.Any(arg => arg == forceShortName || arg == forceName);
+
+            void Delete(DirectoryInfo dir, bool withLockCheck)
+            {
+                if (!force)
+                    return;
+
+                if (withLockCheck) // This may only be good enough for Windows
+                {
+                    var tempPath = Path.Join(dir.Parent.FullName, "_" + dir.Name);
+                    dir.MoveTo(tempPath); // Attempt to rename before deleting
+                    Directory.Delete(tempPath, recursive: true);
+                }
+                else
+                {
+                    dir.Delete(recursive: true);
+                }
+            }
+
+            var tail = args.Except(flags);
+            var minDays = int.TryParse(tail.FirstOrDefault(), out var n) ? n : 30;
             string nugetcache = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\.nuget\\packages\\";
             DirectoryInfo info = new DirectoryInfo(nugetcache);
             long totalDeleted = 0;
@@ -25,18 +46,19 @@ namespace NugetCacheCleaner
                         Console.WriteLine($"{versionFolder.FullName} last accessed {Math.Floor(lastAccessed.TotalDays)} days ago");
                         try
                         {
-                            versionFolder.MoveTo(Path.Combine(versionFolder.Parent.FullName, "_" + versionFolder.Name)); //Attempt to rename before deleting
-                            versionFolder.Delete(true);
+                            Delete(versionFolder, withLockCheck: true);
                             totalDeleted += size;
                         }
                         catch { }
                     }
                 }
                 if (folder.GetDirectories().Length == 0)
-                    folder.Delete(true);
+                    Delete(folder, withLockCheck: false);
             }
             var mbDeleted = (totalDeleted / 1024d / 1024d).ToString("0");
-            Console.WriteLine($"Done! Deleted {mbDeleted} MB.");
+            Console.WriteLine(
+                force ? $"Done! Deleted {mbDeleted} MB."
+                      : $"Will delete {mbDeleted} MB. Re-run with {forceShortName} or {forceName} flag to really delete.");
         }
     }
 }
